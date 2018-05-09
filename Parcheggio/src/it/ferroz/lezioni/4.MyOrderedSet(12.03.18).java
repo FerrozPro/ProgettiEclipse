8d5e9ea077d2fbd9510899dae7d8f3700b08a8de
Elia Ferrotti <elia.ferrotti@capgemini.com>
2018-05-09 11:23:01 +0200

    fix lezione 4
    need to do exercise on that

--- Parcheggio/src/it/ferroz/lezioni/4.MyOrderedSet(12.03.18).java
+++ Parcheggio/src/it/ferroz/lezioni/4.MyOrderedSet(12.03.18).java
@@ -1,16 +1,16 @@
-/*Set != List. Nel Set non ho duplicati, mentre nella List s√¨. 
-Se creo una mia classe MySet, il metodo "insert" dovr√† controllare se il numero che devo inserire esiste gi√† nell'insieme. 
+/*Set != List. Nel Set non ho duplicati, mentre nella List sÏ. 
+Se creo una mia classe MySet, il metodo "insert" dovr‡† controllare se il numero che devo inserire esiste gi‡† nell'insieme. 
 Ma se invece devo inserire un generics?*/
 
 public class MySet<T> implements MyList<T>{
 	...
 	void insertAt(int i, T data){
-		while(...){ //finch√® ci sono nodi
+		while(...){ //finchË ci sono nodi
 			if(n.data == data){...} /*sto confrontanto i puntatori e non sto guardando cosa contengono.
 									es.: se ho due Integer, entrambi uguali a 8, vedo che puntano allo stesso oggetto,
 									ma non vedo che entrambi sono uguali a 8.
 			Quindi dovrei scrivere:*/
-			if(n.data.equals(data){...} /*se data √® di tipo Integer va bene, perch√® equals √® un metodo di Object. 
+			if(n.data.equals(data){...} /*se data Ë di tipo Integer va bene, perchË equals Ë un metodo di Object. 
 										Ma se data ha un tipo diverso, di una classe che ho inventato io, in quella classe devo implementare "equals".
 										Se voglio che sia un insieme di elementi ordinati, quando inserisco, inserisco gli elementi in modo che siano ordinati:
 										controllo l'elemento prima e l'elemento dopo.
@@ -39,18 +39,18 @@ public interface MyComparable{
 
 MyOrderedSet<Cane> a = new MyOrderedSet<Cane>();
 
-//Avr√≤ che la classe Cane sar√†:
+//AvrÚ che la classe Cane sar‡†:
 
 public class Cane implements MyComparable{ //T deve estendere MyComparable
 	String name;
-	int peso, et√†;
+	int peso, et‡†;
 	
 	boolean compare(MyComparable o){
 		return this.peso < o.peso;
 	}
 }
 
-/*Per√≤ MyComparable non ha il campo "peso", quindi non posso scrivere il metodo "compare" cos√¨ --> dovrei fare un cast (o diventa di tipo Cane)
+/*PerÚ MyComparable non ha il campo "peso", quindi non posso scrivere il metodo "compare" cosÏ --> dovrei fare un cast (o diventa di tipo Cane)
 Quindi scrivo:*/
 
 public interface MyComparable<S>{
